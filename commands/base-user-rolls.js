b29309d4a74234f29403973b73be7493b6c92aef
Undermuz <undermuz@gmail.com>
2024-06-07 01:02:26 +0300

    fix

--- commands/base-user-rolls.js
+++ commands/base-user-rolls.js
@@ -54,7 +54,7 @@ const BaseUserRolls = async (interaction, users) => {
 
     results.sort((a, b) => a.value - b.value)
 
-    texts.splice(0, results.length)
+    texts.splice(texts.length - results.length, results.length)
 
     for (let res of results) {
         let r = `${res.value}`
